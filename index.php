d4573425ff3aadb81e9957775dd72a8a89291abf
Abyss <75150797+3399711161@users.noreply.github.com>
2023-05-01 19:15:43 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,121 @@
+<?php
+class class_openai
+
+{
+
+	const ROOT_URL = "https://api.openai.com/";  
+
+    //构造函数，获取Access Token
+
+    public function __construct($apikey = NULL)
+
+    {
+
+        $this->apikey = $apikey;
+
+    }
+
+    //文字完成 上下文
+
+    public function chat_completions_context($messages)
+
+    {
+
+        $field = array(
+
+            "model"=>"gpt-3.5-turbo",
+
+            // "temperature" => 0,
+
+            // "stream" => true,
+
+            "messages"=>$messages,
+
+        );
+
+        $url = self::ROOT_URL."v1/chat/completions";
+
+        
+
+        $response = $this->http_request($url, json_encode($field));
+
+        $result = json_decode($response, true);
+
+        return trim($result["choices"][0]["message"]["content"]);
+
+    }
+
+	//图片完成
+
+    public function images_generations($prompt)
+
+    {
+
+        $field = array("prompt"=>$prompt,
+
+                       "n"=>1,
+
+                       "size"=>"256x256",
+
+                      );
+
+        $url = self::ROOT_URL."v1/images/generations";
+
+        $response = $this->http_request($url, json_encode($field));
+
+        $result = json_decode($response, true);
+
+        return trim($result["data"][0]["url"]);
+
+    }
+
+    //HTTP请求（支持HTTP/HTTPS，支持GET/POST）
+
+    protected function http_request($url, $data = null)
+
+    {
+
+        $headers = array(
+
+            "Content-Type: application/json",
+
+            "Authorization: Bearer " . $this->apikey
+
+        );
+
+        var_dump($url);
+
+        var_dump($headers);
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+        if (!empty($data)){
+
+            curl_setopt($ch, CURLOPT_POST, 1);
+
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        }
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+        $output = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $output;
+
+    }
+
+}
+?>
